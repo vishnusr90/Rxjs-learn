7ad19757c8dd898e25ea46af44844c26848d3ef7
Vishnu <vishnusr01@gmail.com>
2019-01-28 21:39:57 +0800

    add value in dom

--- src/code.ts
+++ src/code.ts
@@ -1,8 +1,15 @@
-import { Observable } from "rxjs/Observable";
+import { Observable } from 'rxjs/Observable';
 
 var observable = Observable.create((observer: any) => {
-    observer.next('Hey !');
+    observer.next("Hey !!!")
 });
 
+observable.subscribe((value: any) => addItem(value));
 
- observable.subscribe((x:any) => console.log(x));
+
+function addItem(val: any){
+    var node = document.createElement("li");
+    var textNode = document.createTextNode(val);
+    node.appendChild(textNode);
+    document.getElementById("output").appendChild(node);
+}
